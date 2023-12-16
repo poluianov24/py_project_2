0871d570d410c598b2966347a18d8ec9793b6c0b
poluianov24 <poluianov.vlad.24@gmail.com>
2023-12-16 23:58:22 +0200

    This is a done lab ex-2

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 try:
-    n_1 = float(input("Enter salary amount: "))
-    n_2 = float(input("Enter the payment amount to the bank: "))
-    n_3 = float(input("Enter utility payment: "))
+    n_1 = float(input("Enter the number: "))
+    n_2 = float(input("Enter percentage: "))
     print("\033[35m")
-    print("Spare cash: ", n_1 - n_2 - n_3)
+    print("Percentage of the number: ", n_1 * n_2 / 100)
 except Exception as e:
     print(e)
