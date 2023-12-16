85b1b2db0d3f824257c138ed49e5ee42f3bb779f
poluianov24 <poluianov.vlad.24@gmail.com>
2023-12-17 00:35:21 +0200

    Fix bug

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ try:
     n_1 = float(input("Enter width: "))
     n_2 = float(input("Enter height: "))
     print("\033[35m")
-    print("Area of a rectangle: ", n_1 * n_2 / 2)
+    print("Area of a rectangle: ", n_1 * n_2)
 except Exception as e:
     print(e)
